e4cc02f335b327a0acdbf8db8c8a7073ac709436
pdung0302 <21010613@st.phenikaa-uni.edu.vn>
2024-04-19 15:31:27 +0700

    update

--- Login.php
+++ Login.php
@@ -6,12 +6,13 @@
     <title>Login</title>
 </head>
 <body>
-    <span style = 'font-weight: bold; color:blue;'> Đăng nhập hệ thống</span>
+    <span style = 'font-weight: bold; color:blue; font-size:20px;' > Đăng nhập hệ thống</span>
+    <br><br>
     <form action="xlLogin.php" method="get">
         Tên đăng nhập: <input type="text" name="username" />
-        <br>
-        Mât khẩu: <input type="password" name="passWord" />
-        <br>
+        <br><br>
+        Mât khẩu: <input type="password" name="passWord"  style ='margin-left:35px;'/>
+        <br><br>
         <input type="submit" value="Đăng nhập" />
     </form>
 </body>
